dfbe337c7fcc153f3c9e853294d879701946391b
Lukyanova-Angelina <st128743@student.spbu.ru>
2024-10-23 20:41:35 +0300

    second commit with deliberate changes

--- Assigment 3/ClassTransformers.cpp
+++ Assigment 3/ClassTransformers.cpp
@@ -1,11 +1,99 @@
 #include <iostream> 
 #include <string>
-class Transformer
-{
-	std::string name;
-	int size, age;
-	bool is_alive;
-public:
-	Transformer(std::string t_name, int t_size, int t_age, bool t_is_alive) : name(t_name), size(t_size), age(t_age), is_alive(t_is_alive) 
-	{}
+#include "ClassTransformers.h"
+// конструкторы и деструкторы
+Transformer::Transformer(std::string name, int power, int hp, int stamina, int x, int y, bool alive, bool injured, bool lr, bool jump): 
+    _Name(name), 
+    _Power(power), 
+    _HP(hp), 
+    _Stamina(stamina), 
+    X(x),
+    Y(y),
+    _Is_alive(alive), 
+    _Is_injured(injured), 
+    _Look_right(lr), 
+    _Is_jump(jump){}
+
+Transformer::~Transformer(){};
+
+// methods
+void Transformer::Move(){
+	if(getLookright() == 1){
+		setX(int getX() + 1);
+	}else{
+		setX(int getX() - 1);
+	}
 };
+	
+void Transformer::Jump(){
+	if (getJump() == 1){
+		setY(int getY() + 1);
+	}else{
+		setY(int getY() - 1);
+	}
+}
+void Transformer::Turn(){
+	if (getLookright == 1){
+		setLookright(0);
+	}else{
+		setLookright(1);
+	}
+}
+
+// Sets
+void setPower(int power){
+	_Power = power;
+}
+void setHP(int hp){
+	_HP = hp;
+}
+void setStamina(int stamina){
+	_Stamina = stamina;
+}
+void setAlive (bool is_alive){
+	_Is_alive = is_alive;
+}
+void setInjured (bool is_injured){
+	_Is_injured = is_injured;
+}
+void setX(int x){
+	X = x;
+}
+void setY(int y){
+	Y = y;
+}
+void setLookright(bool lr){
+	_Look_right = lr;
+}
+void setJump(bool jump){
+	_Is_jump = jump;
+}
+
+// Gets 
+int getPower(){
+	return _Power;
+}
+int getHP(){
+	return _HP;
+}
+int getStamina(){
+	return _Stamina;
+}
+int getX(){
+	return X;
+}
+int getY(){
+	return Y;
+}
+bool getAlive(){
+	return _Is_alive;
+}
+bool getInjured(){
+	return _Is_injured;
+}
+bool getLookright(){
+	return _Look_right;
+}
+bool getJump(){
+	return _Is_jump;
+}
--- Assigment 3/ClassTransformers.h
+++ Assigment 3/ClassTransformers.h
@@ -1,5 +1,47 @@
 #ifndef CLASSTRANSFORMERS
 #define CLASSTRANSFORMERS
+#include <iostream>
 
 
+class Transformer
+{
+private:
+	// это поля
+	std::string _Name;
+	int _Power, _HP, _Stamina, X, Y;
+	bool _Is_alive, _Is_injured, _Look_right, _Is_jump;
+
+public:
+	// Конструктор и деструктор
+	Transformer(std::string name, int power, int hp, int stamina, int x, int y, bool alive, bool injured, bool lr, bool jump);
+	~Transformer();
+
+	// методы
+	void Move();
+	void Jump();
+	void Turn();
+
+	// Sets
+	void setPower(int power);
+	void setHP(int hp);
+	void setStamina(int stamina);
+	void setAlive (bool is_alive);
+	void setInjured (bool is_injured);
+	void setX(int x);
+	void setY(int y);
+	void setLookright(bool lr);
+	void setJump(bool jump);
+
+	// Gets 
+	int getPower();
+	int getHP();
+	int getStamina();
+	int getX();
+	int getY();
+	bool getAlive();
+	bool getInjured();
+	bool getLookright();
+	bool getJump();
+
+};
 #endf
